7b898232050aa9cb0b681c84d04c674839e9105e
cowhca <connorwhcallahan@gmail.com>
2022-12-07 09:44:50 -0800

    day6

---
+++ 2022/day6/part2.js
@@ -0,0 +1,30 @@
+const fs = require("fs/promises");
+
+const main = async () => {
+  let data;
+  try {
+    data = await fs.readFile("input.txt", { encoding: "utf8" });
+  } catch (err) {
+    console.log(err);
+  }
+  let curMarker = "";
+  for (let i = 0; i < data.length; i++) {
+    const letter = data[i];
+    let letterIndex = curMarker.indexOf(letter);
+    if (letterIndex === -1) {
+      curMarker += letter;
+      if (curMarker.length === 14) {
+        console.log(i + 1);
+        break;
+      }
+    } else {
+      curMarker = curMarker.slice(letterIndex + 1) + letter;
+    }
+    if (!curMarker.includes(letter)) {
+      curMarker += letter;
+    } else {
+    }
+  }
+};
+
+main();
